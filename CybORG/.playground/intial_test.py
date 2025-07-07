12449d96f79656700be337b08af26e5296546730
matsevytyi <povilas.kvederas@student.uva.nl>
2025-07-07 17:18:48 +0000

    updated test to use custom scenarios

--- CybORG/.playground/intial_test.py
+++ CybORG/.playground/intial_test.py
@@ -5,8 +5,18 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import inspect
 from CybORG import CybORG
-path = str(inspect.getfile(CybORG))
- path = path[:-10] + '/Shared/Scenarios/Scenario2.yaml'
+
+extended = True
+
+# Locate Scenario2.yaml path
+if extended:
+    # path = "Scenario2.yaml"
+    # path = "Scenario2_Linear.yaml"
+    # path = "Scenario2_Extended.yaml"
+    path = os.path.dirname(__file__) + "/scenarios/Scenario2_Linear.yaml"
+else:
+    path = str(inspect.getfile(CybORG))
+    path = path[:-10] + '/Shared/Scenarios/Scenario2.yaml'
 
 # Create the environment
 cyborg = CybORG(path, 'sim')
