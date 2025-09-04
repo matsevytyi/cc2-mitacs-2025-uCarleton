d49fff0070f0a6b51217a71ca2a8ed2b34bbf4d4
Andrii Matsevytyi <macevitiya@i.ua>
2025-09-03 21:55:13 -0400

    updated training pipeline to handle deployment of different wrappers; solved Gym and Gymnasium mismatch

--- CybORG/.playground/convenient_SST_deployment_example.py
+++ CybORG/.playground/convenient_SST_deployment_example.py
@@ -5,16 +5,26 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from CybORG import CybORG
 from CybORG.Agents import B_lineAgent
 
-from CybORG.Agents.Wrappers import TransformerWrapper
+from CybORG.Agents.Wrappers import TransformerWrapper, ChallengeWrapper
 
-from stable_baselines3 import DQN
+from gymnasium.wrappers.step_api_compatibility import StepAPICompatibility
+from gymnasium.wrappers.compatibility import LegacyEnv
+from gymnasium.wrappers import EnvCompatibility
+
+
+from stable_baselines3 import DQN, PPO
+#from sb3_contrib import C51
+
 
 from stable_baselines3.common.logger import configure
 from RewardLoggingCallback import RewardLoggingCallback
+from MetricLoggingCallback import MetricLoggingCallback
+from stable_baselines3.common.monitor import Monitor
 
 
 # Load cfg
 extended = True
+transformer = False
 
 # Locate Scenario2.yaml path
 if extended:
@@ -32,15 +42,23 @@ else:
 cyborg = CybORG(path, 'sim', agents={'Red': B_lineAgent})
 cyborg.reset()
 
-gym_env = TransformerWrapper(env=cyborg, agent_name='Blue')
+
+if transformer:
+    gym_env = TransformerWrapper(env=cyborg, agent_name='Blue', max_steps=100)
+else:
+    gym_env = ChallengeWrapper(env=cyborg, agent_name='Blue', max_steps=100)
+    gym_env = EnvCompatibility(gym_env)
+    #gym_env = StepAPICompatibility(gym_env, output_truncation_bool=True)
+    
+gym_env = StepAPICompatibility(gym_env, output_truncation_bool=True)
+    
 gym_env.reset()
 
 print(cyborg)
 
 # ppo/c51 - try
 
-# Build model
-model = DQN(
+model1 = DQN(
     policy='MlpPolicy',
     env=gym_env,
     verbose=2,
@@ -50,14 +68,20 @@ model = DQN(
 )
 
 callback = RewardLoggingCallback(verbose=1)
+# callback = MetricLoggingCallback(verbose=1)
 
-log_path = "./logs/DQN_17/"
+log_path = "./logs/"
 new_logger = configure(log_path, ["stdout", "tensorboard"])
-model.set_logger(new_logger)
+model1.set_logger(new_logger)
 
-model.learn(
-    total_timesteps=1000,
-    tb_log_name="DQN_17",
+model1.learn(
+    total_timesteps=100000,
+    tb_log_name="DQN",
     log_interval=10,
     callback=callback  
     )
+
+filename = "dqn_transformer_model_100000" if transformer else "dqn_default_model_100000"
+model1.save(filename)
+
+#print("Metrics:", callback.get_metrics())
