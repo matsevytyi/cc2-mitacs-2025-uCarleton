6010e5927dc980ed5f3968a3761803e18e8203f0
Andrii Matsevytyi <macevitiya@i.ua>
2025-09-20 18:30:03 -0400

    updated SST script with recent hyperparams

--- CybORG/.playground/convenient_SST_deployment_example.py
+++ CybORG/.playground/convenient_SST_deployment_example.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from CybORG import CybORG
 from CybORG.Agents import B_lineAgent
 
-from CybORG.Agents.Wrappers import TransformerWrapper, ChallengeWrapper
+from CybORG.Agents.Wrappers import TransformerWrapper, ChallengeWrapper, PaddingWrapper
 
 from gymnasium.wrappers.step_api_compatibility import StepAPICompatibility
 from gymnasium.wrappers.compatibility import LegacyEnv
@@ -17,21 +17,23 @@ from stable_baselines3 import DQN, PPO
 
 
 from stable_baselines3.common.logger import configure
-from RewardLoggingCallback import RewardLoggingCallback
-from MetricLoggingCallback import MetricLoggingCallback
 from stable_baselines3.common.monitor import Monitor
 
 
 # Load cfg
 extended = True
-transformer = False
+transformer = True
+pad = False
+
+algorithm = DQN
+#algorithm = PPO
+
+# subnet_id = 0
 
 # Locate Scenario2.yaml path
 if extended:
 
     path = "Scenario2.yaml"
-    # path = "Scenario2_Linear.yaml"
-    # path = "Scenario2_Extended.yaml"
 
     path = os.path.dirname(os.path.dirname(__file__)) + "/.playground/scenarios/" + path
 else:
@@ -43,14 +45,15 @@ cyborg = CybORG(path, 'sim', agents={'Red': B_lineAgent})
 cyborg.reset()
 
 
+
 if transformer:
-    gym_env = TransformerWrapper(env=cyborg, agent_name='Blue', max_steps=100)
+    gym_env = TransformerWrapper(raw_cyborg=cyborg, agent_name='Blue', max_steps=50)
+elif pad:
+    gym_env = PaddingWrapper(env=cyborg, agent_name='Blue', max_devices=100, max_steps=50)
 else:
-    gym_env = ChallengeWrapper(env=cyborg, agent_name='Blue', max_steps=100)
+    gym_env = ChallengeWrapper(env=cyborg, agent_name='Blue', max_steps=50)
     gym_env = EnvCompatibility(gym_env)
     #gym_env = StepAPICompatibility(gym_env, output_truncation_bool=True)
-    
-gym_env = StepAPICompatibility(gym_env, output_truncation_bool=True)
     
 gym_env.reset()
 
@@ -58,30 +61,38 @@ print(cyborg)
 
 # ppo/c51 - try
 
-model1 = DQN(
+model1 = algorithm(
     policy='MlpPolicy',
     env=gym_env,
     verbose=2,
     tensorboard_log="./logs/",
     device='cpu',
-    buffer_size=100_000
+    # Replay Buffer
+    buffer_size=500_000,      # Big but fits on T4
+    learning_starts=10_000,   # fill buffer with diversity
+
+    # Training
+    #batch_size=64,        
+    train_freq=4,           
+    gradient_steps=1,         # 1 gradient update per train step
+    target_update_interval=5_000, 
+
+    # Optimization
+    learning_rate=1e-5,       # Default, safe for DQN; lower (5e-5) if unstable
+    gamma=0.99,               
+    exploration_fraction=0.2, # How fast epsilon decays 0.2 > 0.3 > 0.1
+    exploration_final_eps=0.025, 
 )
 
-callback = RewardLoggingCallback(verbose=1)
-# callback = MetricLoggingCallback(verbose=1)
-
-log_path = "./logs/"
-new_logger = configure(log_path, ["stdout", "tensorboard"])
-model1.set_logger(new_logger)
+#run_id = "padding to 0 version on {subnet_id}"
+run_id = "CLS[1, 2*64] + simple obs + FULL subnet + norm + random CLS on 5000 epochs_50 episodes + 2 transformer heads + 4 layers + bigger decay = 0.2 + smaller exploration_final_eps=0.025"
+#run_id = "padding to -1 version"
 
 model1.learn(
-    total_timesteps=100000,
-    tb_log_name="DQN",
-    log_interval=10,
-    callback=callback  
+    total_timesteps=500_000,
+    tb_log_name=f"{algorithm.__name__}_run_{run_id}",
+    log_interval=10
     )
 
-filename = "dqn_transformer_model_100000" if transformer else "dqn_default_model_100000"
+filename = f"dqn_transformer_model_100000_{run_id}" if transformer else "dqn_padding_model_100000"
 model1.save(filename)
-
-#print("Metrics:", callback.get_metrics())
